fe8d854cb8a724a908fe11ebf8e9ae1778092f6b
Steven Hertz <menashehertz@gmail.com>
2017-12-13 23:01:33 -0500

    Initial Commit

---
+++ CreateGoogleSearchURL/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CreateGoogleSearchURL
+//
+//  Created by Steven Hertz on 12/13/17.
+//  Copyright © 2017 Steven Hertz. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
